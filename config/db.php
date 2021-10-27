c943b2e7bb89d0934b8360bc6ce644e3ddf7bb14
Mark Mandryk <0mandrock1@gmail.com>
2021-10-27 11:09:48 +0300

    db config

--- config/db.php
+++ config/db.php
@@ -2,13 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=yii2apptest',
+    'username' => 'yii2apptest',
+    'password' => 'vIfKUVGJ2',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
