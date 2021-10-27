41dc72b903dc11ab2faf5013aa08314ed9ed200b
Mark Mandryk <0mandrock1@gmail.com>
2021-10-27 18:10:21 +0300

    config application component: - routing rules - enable JSON parser

--- config/web.php
+++ config/web.php
@@ -15,6 +15,9 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'VOII1t7HEJFxweqEldD-XEIzXTkRl_8N',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ],
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -43,14 +46,17 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
+            'enableStrictParsing' => true,
             'showScriptName' => false,
             'rules' => [
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'user'],
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'group'],
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
